1b4e02446774b4c276317573f85e7fe25bc3a8f5
kelmpk13 <75197716+kelmpk13@users.noreply.github.com>
2020-11-30 11:21:15 +0000

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,18 @@
+<?php
+//cara penulisan class dan property OOP PHP - @pulung_coding
+class mobil{
+  //property OOP
+  var $warna;
+  var $merek;
+  var $ukuran;
+  
+  //method OOP
+  function maju(){
+    //isi method
+}
+  function berhenti(){
+    //isi method
+  } 
+  
+}
+ ?>
