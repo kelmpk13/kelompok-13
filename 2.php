e3d29fc713f56b3529ab353b8963df73ebd14d39
kelmpk13 <75197716+kelmpk13@users.noreply.github.com>
2020-11-30 11:20:11 +0000

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,10 @@
+<?php
+//cara penulisan class dan property OOP PHP - @pulung_coding 
+class mobil {          //class mobil
+    var $warna;        //property warna
+    var $merek;        //property merek
+    var $ukuran;       //property ukuran
+    
+    
+}
+ ?>
